5c9f1e945de85c22c8822e61d3e7a00dba907eeb
Karimifar <mkarimifar@utlocaldminsPro.utsystem.local>
2018-07-17 16:53:12 -0500

    styling and additional data

--- Public/assets/js/app.js
+++ Public/assets/js/app.js
@@ -12,10 +12,16 @@ var oldWidthB = parseInt($("#B-zipBar").attr("width"))
 var oldWidthH = parseInt($("#H-zipBar").attr("width"))
 var oldWidthW = parseInt($("#W-zipBar").attr("width"))
 
+var allMed = 5.48;
+var bMed = 10.74;
+var hMed = 5.10;
+var wMed = 4.70;
+
 $("#submit-btn").on("click", function(){
 
     
     event.preventDefault();
+    $("#table").attr("class", "visible")
     var userZip = $("#zipcode-input").val().trim();
     var input = {
         zipcode: userZip
@@ -27,7 +33,7 @@ $("#submit-btn").on("click", function(){
 
     .then(function(data){
         
-
+        console.log(data)
         var zipLat = data[0].Lat;
         var zipLng = data[0].Lng;
         var zipCity = data[0].City;
@@ -54,6 +60,11 @@ $("#submit-btn").on("click", function(){
                         userZip,
                     ]
                 }
+
+
+                $("#zip-city").text(zipCity)
+                $("#zip-county").text(zipCounty)
+                $("#where").attr("class","visible")
                 mymap.flyTo([zipLat, zipLng],13);
 
                 console.log(sendData)
@@ -73,6 +84,9 @@ $("#submit-btn").on("click", function(){
 
         }else{
             mymap.flyTo([zipLat, zipLng],13);
+            $("#zip-city").text(zipCity)
+            $("#zip-county").text(zipCounty)
+            $("#where").attr("class","visible")
         }
         
        
@@ -82,6 +96,7 @@ $("#submit-btn").on("click", function(){
         var bImr = data[0].IMR_B;
         var hImr = data[0].IMR_H;
         var wImr = data[0].IMR_W;
+
 
         var newWidthAll = allImr * scale;
         var newWidthB = bImr * scale;
@@ -96,31 +111,51 @@ $("#submit-btn").on("click", function(){
 
         $("#bigNumber").text(allImr)
         $("#thePlace").text("ZIP: "+ userZip)
-
+        
         if(allImr === null){
             $("#allImr-text").text("Not enough data")
+            $("#allImr-text").addClass("null-data")
+            $("#all-birth").text("<400")
+            $("#all-ci").text("X")
         }else{
             $("#allImr-text").text(allImr)
+            $("#all-birth").text(data[0].BirthCat)
+            $("#all-ci").text(data[0].LowerCI + " – " + data[0].UpperCI)
         }
         if(bImr === null){
             $("#bImr-text").text("Not enough data")
+            $("#bImr-text").addClass("null-data")
+            $("#b-birth").text("<400")
+            $("#b-ci").text("X")
         }else{
             $("#bImr-text").text(bImr)
+            $("#b-birth").text(data[0].BirthCat_B)
+            $("#b-ci").text(data[0].LowerCI_B + " – " + data[0].UpperCI_B)
         }
         if(hImr === null){
             $("#hImr-text").text("Not enough data")
+            $("#hImr-text").addClass("null-data")
+            $("#h-birth").text("<400")
+            $("#h-ci").text("X")
         }else{
             $("#hImr-text").text(hImr)
+            $("#h-birth").text(data[0].BirthCat_H)
+            $("#h-ci").text(data[0].LowerCI_H + " – " + data[0].UpperCI_H)
         }
         if(wImr === null){
             $("#wImr-text").text("Not enough data")
+            $("#wImr-text").addClass("null-data")
+            $("#w-birth").text("<400")
+            $("#w-ci").text("X")
         }else{
             $("#wImr-text").text(wImr)
+            $("#w-birth").text(data[0].BirthCat_W)
+            $("#w-ci").text(data[0].LowerCI_W + " – " + data[0].UpperCI_W)
         }
 
 
         var graphTl = new TimelineMax();
-        graphTl.to("#All-zipBar", 1, {scaleX: newWidthAll/oldWidthAll, transformOrigin: "0% 50%" })
+        graphTl.to("#All-zipBar", 1, {scaleX: newWidthAll/oldWidthAll, transformOrigin: "0% 50%"})
         .to("#allImr-text", 1, {x: newWidthAll + 93}, "=-1")
         .to("#B-zipBar", 1, {scaleX: newWidthB/oldWidthB, transformOrigin: "0% 50%" }, "=-1")
         .to("#bImr-text", 1, {x: newWidthB + 93}, "=-1")
@@ -149,3 +184,8 @@ openingAnimation();
 
 
 
+function makeTable(flag){
+    if(!flag){
+
+    }
+}
